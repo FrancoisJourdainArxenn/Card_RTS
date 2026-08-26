ee88c90cba7f7671b7e55068b462f6dd874b7472
François Jourdain <jourdain.f@live.fr>
2026-08-26 16:48:29 +0200

    Tweak(Cards): cards changes, values stats & effects. Fix(missing): tier 4 & 5 icons will show up on cards.

--- Assets/Scripts/Visual_Script/Managers/VisualManager.cs
+++ Assets/Scripts/Visual_Script/Managers/VisualManager.cs
@@ -41,7 +41,7 @@ public class VisualManager : MonoBehaviour
     public float CameraShakeDurationStrong = 0.3f;
 
     [Header("Card Tier Icons")]
-    public Sprite[] CardTierIcons = new Sprite[3]; // index 0 = T1, 1 = T2, 2 = T3
+    public Sprite[] CardTierIcons = new Sprite[5]; // index 0 = T1, 1 = T2, 2 = T3, 3 = T4, 4 = T5
 
     void Awake()
     {
